15b4677b58b97b63cb37b2357f38d876958d245d
Aleksey Apanas <lexaapanas@mail.ru>
2023-03-17 14:16:45 +0300

    create bd for user and product

--- postgresql/ddl/03_recipe.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS fitness.recipe
-(
-    uuid uuid NOT NULL,
-    dt_create timestamp without time zone,
-    dt_update timestamp without time zone,
-    title character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT recipe_pkey PRIMARY KEY (uuid)
-)
